229b44e8fd2e652b8e7a9c53461624b4dbf3924b
Kozar Sviatoslav <s.kozar@clearjunction.com>
2020-03-29 19:06:33 +0300

    db structure init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TextAnalysis
+
+To initialize docker project to start develop
+please take a look on 
+`_tools/docer/README.md`
+
+To import db structure use next command in container:
+`psql -d text_analysis_db rev_user -a -f db_structure.sql`
--- import/db_structure.sql
+++ import/db_structure.sql
@@ -3,14 +3,17 @@ CREATE table strongs_dictionary (
     number varchar(50) not null,
     type varchar(50) not null
 );
+CREATE unique index strong_index
+    on strongs_dictionary(number,type);
 CREATE table languages (
     id serial primary key,
     title varchar(200) not null
 );
 CREATE table strong_values (
     id serial primary key,
+    value varchar(300) not null,
     language_id integer not null,
-    strongs_dictionary_id varchar(200) not null,
+    strongs_dictionary_id integer not null,
     CONSTRAINT languages_language_id_fkey foreign key (language_id)
         references languages (id) match simple
         on update no action on delete no action,
@@ -18,6 +21,4 @@ CREATE table strong_values (
         references strongs_dictionary (id) match simple
         on update no action on delete no action
 );
-CREATE unique index strong_index
-on strongs_dictionary(number,type);
 
