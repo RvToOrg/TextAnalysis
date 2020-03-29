24c89f672ab0ba91792bc258e80d7cac085f5a93
Kozar Sviatoslav <s.kozar@clearjunction.com>
2020-03-29 19:24:34 +0300

    db structure done

--- import/db_structure.sql
+++ import/db_structure.sql
@@ -1,3 +1,12 @@
+-- COMMON TABLES --
+CREATE table languages (
+    id serial primary key,
+    title varchar(200) not null
+);
+
+
+
+-- TABLE SPACE FOR STRONG NUMBERS --
 CREATE table strongs_dictionary (
     id serial PRIMARY Key,
     number varchar(50) not null,
@@ -5,10 +14,6 @@ CREATE table strongs_dictionary (
 );
 CREATE unique index strong_index
     on strongs_dictionary(number,type);
-CREATE table languages (
-    id serial primary key,
-    title varchar(200) not null
-);
 CREATE table strong_values (
     id serial primary key,
     value varchar(300) not null,
@@ -21,4 +26,26 @@ CREATE table strong_values (
         references strongs_dictionary (id) match simple
         on update no action on delete no action
 );
+
+
+-- TABLE SPACE FOR DATA SAMPLES --
+create table analysis_bible_canonical_fragmentation(
+    id serial primary key,
+    code varchar(200),
+    canonical_type varchar(200)
+);
+
+CREATE table analysis_excerpts_purposes(
+    id serial primary key,
+    title varchar(100) not null,
+    description varchar(200) not null
+);
+
+create table analysis_words(
+    id serial primary key,
+    strong_id integer,
+    fragment_id integer,
+    excerpt_id integer,
+    value varchar(200)
+)
 
