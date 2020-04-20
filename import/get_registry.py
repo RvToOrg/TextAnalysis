eabfad8767b8c4265acf363c5990094297f8478d
Kozar Sviatoslav <s.kozar@clearjunction.com>
2020-04-20 23:16:08 +0300

    Removed rolads for registry if already exist in cache

--- import/get_registry.py
+++ import/get_registry.py
@@ -1,32 +1,86 @@
+import getopt, sys, os
 import requests, zipfile
 import json
 
+# GET NAME OF MODULE FROM PARAMS
+short_options = "r"
+long_options = ["reload"]
+
+
+reload = False
+full_cmd_arguments = sys.argv
+argument_list = full_cmd_arguments[1:]
+
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+except getopt.error as err:
+    # Output error, and return with an error code
+    print(str(err))
+    sys.exit(2)
+
+
+for current_argument, current_value in arguments:
+    if current_argument in ("-r", "--reload"):
+        reload = True
+        print("Reload registry from source")
 
 
 zip_file_target = './import/registry/registry.zip'
 zip_registry_extract_target = './import/registry'
 json_registry_file = './import/registry/registry.json'
-url = 'http://myb1ble.1nterb1bl1a.org/reg1stry.z1p'
-url = url.replace('1', 'i')
+cache_registry_file_exist = False
+
+try:
+    f = open(json_registry_file)
+    cache_registry_file_exist=True
+except IOError:
+    print("No registry found in cache")
+finally:
+    f.close()
 
 
-#GET REGISTRY FROM
-response = requests.get(url,allow_redirects=True)
+if reload:
 
-#UNZIP REGISTRY
-with open(zip_file_target,'wb') as zfile:
-    zfile.write(response.content)
+    if cache_registry_file_exist:
+        rfile = open(json_registry_file, 'r')
+        json_registry_content = json.loads(rfile.read().encode().decode('utf-8-sig'))
+        url = json_registry_content['registries']
+        url.sort(key=lambda el: el['priority'])
+        url = [x['url'] for x in url if 'test' not in x or not x['test']]
+    else:
+        url = 'http://myb1ble.1nterb1bl1a.org/reg1stry.z1p'
+        url = [url.replace('1', 'i')]
 
-with zipfile.ZipFile(zip_file_target, 'r') as zip_reg:
-    zip_reg.extractall(zip_registry_extract_target)
 
-#BEAUTIFY JSON STRUCTURE OF REGISTRY
-rfile = open(json_registry_file,'r')
-json_registry_content = json.loads(rfile.read().encode().decode('utf-8-sig'))
-rfile.close()
-rfile = open(json_registry_file,'w')
+    #GET REGISTRY FROM
+    i = 0
+    response = requests.get(url[i])
+    print('try url '+url[i])
+    while not response.ok:
+        i += 1
+        print('try url '+ url[i])
+        response = requests.get(url[i])
 
-rfile.write(json.dumps(json_registry_content,sort_keys=True, indent=4))
+    if not response.ok:
+        print('Unable to load registry. Please contact administrator')
+        sys.exit(2)
+
+    #UNZIP REGISTRY
+    with open(zip_file_target,'wb') as zfile:
+        zfile.write(response.content)
+
+    with zipfile.ZipFile(zip_file_target, 'r') as zip_reg:
+        zip_reg.extractall(zip_registry_extract_target)
+
+    #BEAUTIFY JSON STRUCTURE OF REGISTRY
+    rfile = open(json_registry_file,'r')
+    json_registry_content = json.loads(rfile.read().encode().decode('utf-8-sig'))
+    rfile.close()
+    rfile = open(json_registry_file,'w')
+
+    rfile.write(json.dumps(json_registry_content,sort_keys=True, indent=4))
+else:
+    print('Registry already exist in cache. To reload please specify -r or --reload in options')
 
 
 
