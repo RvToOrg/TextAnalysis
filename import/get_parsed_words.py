fad4e2351b9b6fbd3cc4769efa8aa6ca4a972cef
Kozar Sviatoslav <s.kozar@clearjunction.com>
2020-04-21 15:02:39 +0300

    text parsed

--- import/get_parsed_words.py
+++ import/get_parsed_words.py
@@ -1,6 +1,7 @@
 import getopt, sys, os
 import requests, zipfile
-import json
+import json, re
+from peewee import *
 
 
 # GET NAME OF MODULE FROM PARAMS
@@ -36,8 +37,94 @@ db_name = module_name
 for s in replace:
     db_name = db_name.replace(s,replace[s])
 db_name = db_name.lower()
+print('DB used '+db_name)
+
+db_configs = './import/configs/db_config.json'
+db_configs = json.loads(open(db_configs,'r').read().encode().decode('utf-8-sig'))
+
+
+# DB STRUCTURE
+psql_db = PostgresqlDatabase(db_name, user=db_configs['user'],host=db_configs['host'],port=db_configs['port'],password=db_configs['pass'])
+
+class BaseModel(Model):
+    class Meta:
+        database = psql_db
+
+class Verse(BaseModel):
+    book_number=DecimalField()
+    chapter=DecimalField()
+    verse=DecimalField()
+    text=TextField(null=True)
+
+    class Meta:
+        primary_key = False
+        db_table='verses'
+
+class Word(BaseModel):
+    book_number=IntegerField()
+    chapter=IntegerField()
+    verse=IntegerField()
+    word_number=IntegerField()
+    words=FixedCharField(null=True, max_length=256)
+    strong=IntegerField(null=True)
+
+    class Meta:
+        primary_key = False
+        db_table='words'
+
+
+# CREATE TABLE WORDS IF NOT EXIST
+try:
+    psql_db.connect()
+    Word.drop_table()
+    Word.create_table()
+except InternalError as px:
+    print(str(px))
+print('Words table dropped and created')
+
+# sys.exit(0)
 
 
 # PARSE MODULE VERSE TO WORDS BY STRONG
+limit= 200
+verses_count = Verse.select().count()
+print('Total count of verses: {}'.format(verses_count))
 
+
+parsed = 0
+while parsed<verses_count:
+    verses = Verse.select().limit(limit).offset(parsed).execute()
+    data = []
+    for verse in verses:
+        parsed+=1
+        # remove bad tags
+        text = re.sub(r'(<[ikhfnm]>.*?</[ikhfnm]>)?(<pb/>)?(<br/>)?(</?[teJ]>)?','',verse.text, flags=re.IGNORECASE)
+        # cleanup text from bad tags
+        elements =[re.split(r'<s>',x.strip(),flags=re.IGNORECASE) for x in re.split(r'</s>',text, flags=re.IGNORECASE) if x.strip()]
+
+        if parsed % 100 == 0:
+            print('{} verses parsed from {}'.format(parsed,verses_count))
+        number = 0
+        for el in elements:
+            data.append({
+                "book_number":verse.book_number,
+                "chapter":verse.chapter,
+                "verse":verse.verse,
+                "word_number":number,
+                "words":el[0].strip(),
+                "strong":int(el[1].strip()) if len(el)>1 else None,
+            })
+            # row = Word(
+            #     book_number=verse.book_number,
+            #     chapter=verse.chapter,
+            #     verse=verse.verse,
+            #     word_number=number,
+            #     words=el[0].strip(),
+            #     strong= int(el[1].strip()) if len(el)>1 else None
+            # )
+            # row.save()
+            number+=1
+    Word.insert_many(data).execute()
+
+print('Verses parse finished.')
 
